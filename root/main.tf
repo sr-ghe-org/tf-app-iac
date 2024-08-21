5b4fa09b5e34cd3fc342889238700d23d5b3775d
Manish Jindal <manishjindal@google.com>
2024-08-21 14:05:37 +0530

    fix: build isssue

--- root/main.tf
+++ root/main.tf
@@ -1,10 +1,17 @@
+locals {
+  ce_sa = {
+  email  = google_service_account.default.email
+  scopes = ["cloud-platform"]
+}
+}
+
 
 
 module "my-instance-template" {
   source               = "app.terraform.io/sr-test-org/vm-instance-template/gcp"
   version = "0.0.1"
   name_prefix          = "my-it"
-  service_account      = google_service_account.default.email
+  service_account      = local.ce_sa
   project_id           = var.project_id
   source_image_project = var.source_image_project
   source_image         = var.source_image
