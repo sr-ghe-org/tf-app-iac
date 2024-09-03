5a97167c018715bb437e53783e11af54997dc21a
Manish Jindal <jindal.manishkumar1@gmail.com>
2024-09-03 15:00:18 +0530

    create mig (#9)
    * create mig
    
    * dummy commit
    
    * fix: reference right module
    
    ---------
    
    Co-authored-by: Manish Jindal <manishjindal@google.com>

--- root/main.tf
+++ root/main.tf
@@ -7,10 +7,10 @@ locals {
 
 
 
-module "my-instance-template" {
+module "example-it" {
   source               = "app.terraform.io/sr-test-org/vm-instance-template/gcp"
   version              = "0.0.1"
-  name_prefix          = "my-it"
+  name_prefix          = "example-it"
   service_account      = local.ce_sa
   project_id           = var.project_id
   source_image_project = var.source_image_project
@@ -20,4 +20,22 @@ module "my-instance-template" {
   subnetwork           = var.subnetwork
   subnetwork_project   = var.subnetwork_project
   depends_on           = [google_project_iam_member.tfc_sa_role_binding]
+}
+
+module "example-mig" {
+  source              = "app.terraform.io/sr-test-org/vm-managed-instance-group/gcp"
+  version             = "0.0.1"
+  hostname            = "example-mig"
+  project_id          = var.project_id
+  region              = var.region
+  instance_template   = module.example-it.self_link
+  autoscaling_enabled = false
+  max_replicas        = 1
+  min_replicas        = 1
+  autoscaling_cpu     = [{ target = 0.6, predictive_method = "NONE" }]
+  named_ports = [
+    {
+      port = 80
+      name = "http"
+    }]
 }
