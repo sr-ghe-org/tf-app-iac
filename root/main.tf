92675c27a9fe7d1ab1fffcc3c8ba2617afeee4bc
Manish Jindal <manishjindal@google.com>
2024-09-10 16:25:59 +0530

    use startup script to mount netapp vol

--- root/main.tf
+++ root/main.tf
@@ -3,18 +3,25 @@ locals {
     email  = google_service_account.default.email
     scopes = ["cloud-platform"]
   }
+
+  metadata_startup_script =  <<EOF
+    sudo yum install -y nfs-utils
+    sudo mkdir ${google_netapp_volume.test_volume.mount_options.export}
+    sudo mount -t nfs -o rw,hard,rsize=65536,wsize=65536,vers=3,tcp ${google_netapp_volume.test_volume.mount_options.export_full} ${google_netapp_volume.test_volume.mount_options.export}
+  EOF
 }
 
 
 
 module "example-it" {
   source               = "app.terraform.io/sr-test-org/vm-instance-template/gcp"
-  version              = "0.0.1"
+  version              = "0.0.2"
   name_prefix          = "example-it"
   service_account      = local.ce_sa
   project_id           = var.project_id
   source_image_project = var.source_image_project
   source_image         = var.source_image
+  startup_script       = local.metadata_startup_script
   region               = var.region
   subnetwork           = var.subnetwork
   subnetwork_project   = var.subnetwork_project
