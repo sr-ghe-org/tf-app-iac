0ee082c2700b98e9ee88476d4e099eb79f9ffe86
Manish Jindal <manishjindal@google.com>
2024-09-10 10:37:05 +0530

    create gcnv

---
+++ root/gcnv.tf
@@ -0,0 +1,26 @@
+resource "google_netapp_storage_pool" "default" {
+  name = "test-pool"
+  location = "us-central1"
+  service_level = "PREMIUM"
+  capacity_gib = "2048"
+  network = var.network
+}
+
+resource "google_netapp_volume" "test_volume" {
+  location = "us-central1"
+  name = "test-volume"
+  capacity_gib = "100"
+  share_name = "test-volume"
+  storage_pool = google_netapp_storage_pool.default.name
+  protocols = ["NFSV3"]
+  deletion_policy = "DEFAULT"
+  export_policy {
+    rules {
+      allowed_clients = "0.0.0.0/0"
+      has_root_access = true
+      access_type = "READ_WRITE"
+      nfsv3 = true
+      #nfsv4 = true
+    }
+  }
+}
