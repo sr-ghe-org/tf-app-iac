1f2001a3ac660845bfa6d15664ce2c4ec6224768
Manish Jindal <manishjindal@google.com>
2024-09-10 22:03:41 +0530

    remove commented code

--- root/gcnv.tf
+++ root/gcnv.tf
@@ -1,39 +1,3 @@
-/*
-data "google_compute_network" "network" {
-  name    = var.network
-  project = var.subnetwork_project
-}
-
-resource "google_netapp_storage_pool" "default" {
-  project = var.project_id
-  name = "test-pool"
-  location = "us-central1"
-  service_level = "PREMIUM"
-  capacity_gib = "2048"
-  network = data.google_compute_network.network.id
-}
-
-resource "google_netapp_volume" "test_volume" {
-  project = var.project_id
-  location = "us-central1"
-  name = "test-volume"
-  capacity_gib = "100"
-  share_name = "test-volume"
-  storage_pool = google_netapp_storage_pool.default.name
-  protocols = ["NFSV3"]
-  deletion_policy = "DEFAULT"
-  export_policy {
-    rules {
-      allowed_clients = "0.0.0.0/0"
-      has_root_access = true
-      access_type = "READ_WRITE"
-      nfsv3 = true
-      #nfsv4 = true
-    }
-  }
-}
-*/
-
 module "gcnv" {
   source     = "app.terraform.io/sr-test-org/netapp-volumes/gcp"
   version    = "0.0.1"
